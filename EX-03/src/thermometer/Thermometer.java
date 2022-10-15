8b4dd16ef2d5927f51fb318f0ed91ac4436caaa2
cedri <cedri@DESKTOP-L6UGAKJ>
2022-10-15 20:30:53 +0200

    thermometer and car

--- EX-03/src/thermometer/Thermometer.java
+++ EX-03/src/thermometer/Thermometer.java
@@ -1,5 +1,18 @@
 package thermometer;
 
 public class Thermometer {
-
+	public static double DEFAULT_TEMPERATURE = 37.0;
+	
+	double temperature;
+	
+	public Thermometer() {
+		temperature = DEFAULT_TEMPERATURE;
+	}
+	
+	public double getTemperature() {
+		return temperature;
+	}
+	public void reset() {
+		temperature = DEFAULT_TEMPERATURE;
+	}
 }
--- EX-03/src/vehicles/Car.java
+++ EX-03/src/vehicles/Car.java
@@ -1,5 +1,38 @@
 package vehicles;
 
-public class Car {
+import java.time.Period;
+import java.time.Year;
 
+public class Car {
+	public static Period DURRATION_TO_BE_ANTIQUE = Period.ofYears(45);
+	
+	String brand;
+	String model;
+	Year vintage;
+	
+		
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public Year getVintage() {
+		return vintage;
+	}
+	public void setVintage(Year vintage) {
+		this.vintage = vintage;
+	}
+	
+	public boolean isAntique() {
+		return vintage.isBefore(Year.now().minus(DURRATION_TO_BE_ANTIQUE));
+	}
+	
+	
 }
